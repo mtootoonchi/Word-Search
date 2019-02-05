0bafda56a214b49a35dcd5287d0de2c578983170
Matthew Faraz Tootoonchi <44936371+mtootoonchi@users.noreply.github.com>
2019-02-05 10:37:57 -0800

    Add files via upload

---
+++ WordSearchOG/Words.java
@@ -0,0 +1,19 @@
+public class Words
+{
+    private String a;
+    private int length;
+    
+public Words(String a)
+{
+    this.a=a;
+    length=a.length();
+}
+public int length()
+{
+    return length;
+}
+public String toString()
+{
+    return a;
+}
+}
